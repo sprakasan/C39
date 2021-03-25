dec0fc66f4392c4616a8472b89ac1c9fc8b37f61
sprakasan <76974224+sprakasan@users.noreply.github.com>
2021-03-25 16:55:21 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var gameState = 0
+var playerCount 
+var database
+var form, player, game
+var allPlayers
+var distance = 0
+var car1, car2, car3, car4,cars
+function setup(){
+    database=firebase.database();
+    createCanvas(displayWidth, displayHeight);
+
+    game = new Game()
+    game.getState()
+    game.start()
+
+}
+
+function draw(){
+    background("white");
+
+
+
+  if (playerCount === 4){
+      game.update(1)
+  }
+  if(gameState === 1){
+      clear()
+      game.play()
+  }
+}
+
+
+
